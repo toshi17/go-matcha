7a13a48e137e0204be458d02c56153f78f2bca6c
Toshiyuki Oike <toshiyuki_oike@voyagegroup.com>
2022-08-21 09:47:11 +0900

    add websocket handler

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 )
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+	defer conn.CloseHandler()
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		if string(message) == "ping" {
+			message = []byte("pong")
+		}
+		err = conn.WriteMessage(mt, message)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
 	})
 	r.Run()
 }
